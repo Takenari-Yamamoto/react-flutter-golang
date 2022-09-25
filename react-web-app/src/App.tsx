f6fbc18cbf4951857109099556f57e44747ca9fd
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-09-25 16:36:58 +0900

    ui 作ったよ

--- react-web-app/src/App.tsx
+++ react-web-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/common/Header';
 import Add from './components/todo/Add';
 import Detail from './components/todo/Detail';
+import Item from './components/todo/Item';
 import List from './components/todo/List';
 import style from './styles/todo.module.css';
 
@@ -10,11 +11,20 @@ function App() {
     <div className="App">
       <Header />
       <div className={style.container}>
-        <div className={style.todoContainer}>
-          <List />
-          <Detail />
+        {/* Side Bar */}
+        <div className={style.sideBar}>Side Bar</div>
+        <div className={style.todoListContainer}>
+          <div className={style.list}>
+            <h1>Todo Lists</h1>
+            {[1, 2, 3, 4, 5].map((todo, index) => {
+              return <Item />;
+            })}
+            <Add />
+          </div>
+          <div className={style.detail}>
+            <Detail />
+          </div>
         </div>
-        <Add />
       </div>
     </div>
   );
--- react-web-app/src/components/todo/Item.tsx
+++ react-web-app/src/components/todo/Item.tsx
@@ -1,7 +1,22 @@
 import React, { memo } from 'react';
+import style from '../../styles/todo.module.css';
+import checkBoxStyle from '../../styles/checkBox.module.css';
 
 const Item = () => {
-  return <div>Todo</div>;
+  return (
+    <div className={style.todoItem}>
+      <label className={checkBoxStyle.ECM_CheckboxInput}>
+        <input
+          className={checkBoxStyle.ECM_CheckboxInput_Input}
+          type="checkbox"
+        />
+        <span className={checkBoxStyle.ECM_CheckboxInput_DummyInput}></span>
+        <span className={checkBoxStyle.ECM_CheckboxInput_LabelText}>
+          ご飯作る
+        </span>
+      </label>
+    </div>
+  );
 };
 
 export default memo(Item);

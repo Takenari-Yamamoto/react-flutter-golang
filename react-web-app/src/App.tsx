543285a48a7d161ce99cf1ec70ba4e2cee5ce6b3
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-10-01 08:55:11 +0900

    モックにした

--- react-web-app/src/App.tsx
+++ react-web-app/src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/common/Header';
 import Add from './components/todo/Add';
 import Detail from './components/todo/Detail';
 import Item from './components/todo/Item';
 import { useTodo } from './hooks/useTodo';
+import { Todo } from './model/Todo';
 import style from './styles/todo.module.css';
 
 function App() {
   const { todoList, selected, handleSelect } = useTodo();
+
+  const [todoListDemo, setTodoListDemo] = useState<Todo[]>([]);
+  const [text, setText] = useState('');
+  const handleInput = (e: string) => setText(e);
+  const clickAdd = () => {
+    setTodoListDemo([
+      ...todoListDemo,
+      {
+        id: 'werwer',
+        title: text,
+        detail: '',
+        createdAt: new Date(),
+      },
+    ]);
+
+    console.log(todoListDemo);
+  };
 
   return (
     <div className="App">
@@ -16,9 +34,9 @@ function App() {
         <div className={style.sideBar}>Side Bar</div>
         <div className={style.todoListContainer}>
           <div className={style.list}>
-            <Add />
+            <Add clickAdd={clickAdd} handleInput={handleInput} />
             <h1>Todo Lists</h1>
-            {todoList.map((todo, index) => {
+            {todoListDemo.map((todo, index) => {
               return (
                 <Item
                   title={todo.title}
--- react-web-app/src/components/todo/Add.tsx
+++ react-web-app/src/components/todo/Add.tsx
@@ -1,11 +1,22 @@
 import React, { memo } from 'react';
 import style from '../../styles/todo.module.css';
 
-const Add = () => {
+interface Props {
+  handleInput: (e: string) => void;
+  clickAdd: () => void;
+}
+
+const Add = (props: Props) => {
+  const { handleInput, clickAdd } = props;
   return (
     <div className={style.add}>
-      <input className={style.input} />
-      <button className={style.addButton}>追加</button>
+      <input
+        className={style.input}
+        onChange={(e) => handleInput(e.target.value)}
+      />
+      <button className={style.addButton} onClick={clickAdd}>
+        追加
+      </button>
     </div>
   );
 };

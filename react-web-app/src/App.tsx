15bb060fce18a7c58065b0db04ef09a8fd7a7b3f
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-10-01 13:30:11 +0900

    wip: react

--- react-web-app/src/App.tsx
+++ react-web-app/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MobilePage } from './components/page/MobilePage';
 import PcPage from './components/page/PcPage';
+import style from './styles/button.module.css';
 
 function App() {
+  const [screenType, setType] = useState<'pc' | 'mobile'>('mobile');
+  const changeType = (type: 'pc' | 'mobile') => {
+    if (type === 'pc') {
+      setType('mobile');
+      return;
+    }
+    setType('pc');
+  };
   return (
     <div className="App">
-      <PcPage />
+      {screenType === 'pc' && <PcPage />}
+      {screenType === 'mobile' && <MobilePage />}
+      {/* <button className={style.button} onClick={() => changeType(screenType)}>
+        {screenType}
+      </button> */}
     </div>
   );
 }
--- react-web-app/src/components/page/MobilePage.tsx
+++ react-web-app/src/components/page/MobilePage.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import style from '../../styles/mobile/mobile.module.css';
 
 export const MobilePage = () => {
-  return <div>mobiel</div>;
+  return (
+    <div>
+      <header>Sample Todo</header>
+      <footer className={style.footer}>aa</footer>
+    </div>
+  );
 };
